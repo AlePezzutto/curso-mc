426d364aed1cd980388d29db543507a0a3268dee
alepezzutto <ale.pezzutto@gmail.com>
2019-06-20 23:25:26 -0300

    CategoriaDTO

--- src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.nelioalves.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -47,6 +48,10 @@ public class CategoriaService {
 		catch(DataIntegrityViolationException ex) {
 			throw new DataIntegrityException("Não é possível deletar uma categoria que possui produtos");
 		}
+	}
+
+	public List<Categoria> findAll() {
+		return repo.findAll();
 	}
 	
 }
