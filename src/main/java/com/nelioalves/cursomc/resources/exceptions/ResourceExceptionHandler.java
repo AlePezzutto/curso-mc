678abeccb2c3e1fab8db0826a1369754a28350ca
alepezzutto <ale.pezzutto@gmail.com>
2019-06-21 11:17:41 -0300

    Personalizando as mensagem de validação dos campos

--- src/main/java/com/nelioalves/cursomc/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/nelioalves/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -4,6 +4,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -38,4 +40,24 @@ public class ResourceExceptionHandler {
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 		
 	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> validationError(MethodArgumentNotValidException e, 
+			                                            HttpServletRequest request ) {
+		
+		
+		ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value() 
+				                                 ,"Erro de validação de campos" 
+				                                 ,System.currentTimeMillis());
+		
+		// Atribui os erros
+		for(FieldError fieldErr : e.getBindingResult().getFieldErrors()) {
+
+			err.addError(new FieldErrorMessage(fieldErr.getField(), fieldErr.getDefaultMessage()));
+		}
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+		
+	}
+
 }
