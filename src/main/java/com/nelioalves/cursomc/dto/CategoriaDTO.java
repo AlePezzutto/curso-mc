c163791098fee0ccd286a9d82fa252482fda9667
alepezzutto <ale.pezzutto@gmail.com>
2019-06-21 10:54:58 -0300

    Incluindo validação das categorias

--- src/main/java/com/nelioalves/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/nelioalves/cursomc/dto/CategoriaDTO.java
@@ -2,6 +2,10 @@ package com.nelioalves.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.nelioalves.cursomc.domain.Categoria;
 
 public class CategoriaDTO implements Serializable{
@@ -12,6 +16,8 @@ public class CategoriaDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
+	@NotEmpty(message="Preenchimento Obrigatório")
+	@Length(min=5, max=80, message="Campo deve ter entre 5 e 80 caracteres")
 	private String nome;
 	
 	public CategoriaDTO() {
@@ -41,4 +47,7 @@ public class CategoriaDTO implements Serializable{
 		this.nome = nome;
 	}
 	
+	public Categoria toCategoria() {
+		return new Categoria(this.id, this.nome);
+	}
 }
--- src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
@@ -4,6 +4,8 @@ import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
@@ -37,19 +39,19 @@ public class CategoriaResource {
 	}
 
 	@PostMapping
-	public ResponseEntity<Categoria> insert(@RequestBody Categoria categoria){
+	public ResponseEntity<Categoria> insert(@Valid @RequestBody CategoriaDTO catDTO){
 		
-		categoria = service.insert(categoria);
+		Categoria categoria = service.insert(catDTO.toCategoria());
 		
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(categoria.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
 	
 	@PutMapping(value="{id}")
-	public ResponseEntity<Void> update(@RequestBody Categoria categoria
-			                       ,@PathVariable Integer id){
-		categoria.setId(id);
-		categoria = service.update(categoria);
+	public ResponseEntity<Void> update(@Valid @RequestBody CategoriaDTO catDTO
+			                          ,@PathVariable Integer id){
+		catDTO.setId(id);
+		Categoria categoria = service.update(catDTO.toCategoria());
 		
 		return ResponseEntity.noContent().build();
 	}
