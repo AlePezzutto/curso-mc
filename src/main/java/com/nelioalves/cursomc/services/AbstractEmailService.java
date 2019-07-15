19d9be7572ed6d8a6e679675be9fa111f9ec9db1
alepezzutto <ale.pezzutto@gmail.com>
2019-07-15 17:08:10 -0300

    HTML Email

--- src/main/java/com/nelioalves/cursomc/services/AbstractEmailService.java
+++ src/main/java/com/nelioalves/cursomc/services/AbstractEmailService.java
@@ -2,13 +2,27 @@ package com.nelioalves.cursomc.services;
 
 import java.util.Date;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 import com.nelioalves.cursomc.domain.Pedido;
 
 public abstract class AbstractEmailService implements EmailService {
 
+	@Autowired
+	private TemplateEngine templateEngine;
+	
+	@Autowired 
+	private JavaMailSender javaMailSender;
+	
 	@Value("${default.email.sender}")
 	private String defaultSender;
 	
@@ -18,7 +32,23 @@ public abstract class AbstractEmailService implements EmailService {
 		SimpleMailMessage sm = prepareSimpleMailMessageFromPedido(pedido);
 		sendEmail(sm);
 	}
+		
+	@Override
+	public void sendOrderConfirmationHtmlEmail(Pedido pedido) {
+		
+		MimeMessage mm;
+		
+		try {
+			mm = prepareMimeMessageFromPedido(pedido);
+			sendHtmlEmail(mm);
+		} 
+		catch (MessagingException e) {
+			sendOrderConfirmationEmail(pedido);
+		}
+		
+	}
 	
+
 	protected SimpleMailMessage prepareSimpleMailMessageFromPedido(Pedido pedido) {
 
 		SimpleMailMessage sm = new SimpleMailMessage();
@@ -31,4 +61,28 @@ public abstract class AbstractEmailService implements EmailService {
 		
 		return sm;
 	}
+	
+	protected MimeMessage prepareMimeMessageFromPedido(Pedido pedido) throws MessagingException {
+		
+		MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+		MimeMessageHelper mmh = new MimeMessageHelper(mimeMessage, true);
+		
+		mmh.setTo(pedido.getCliente().getEmail());
+		mmh.setFrom(defaultSender);
+		mmh.setSubject("Pedido " + pedido.getId() + " Confirmado. ");
+		mmh.setSentDate(new Date(System.currentTimeMillis()));
+		mmh.setText(htmlFromTemplatePedido(pedido));
+		
+		return mimeMessage;
+		
+	}
+	
+	protected String htmlFromTemplatePedido(Pedido pedido) {
+		
+		Context ctx = new Context();
+		ctx.setVariable("pedido", pedido);
+
+		return templateEngine.process("/email/ConfirmacaoPedido", ctx);
+	}
+	
 }
